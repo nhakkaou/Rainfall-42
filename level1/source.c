91b31c4bcea12dd63186bf12bfd2da880b8b404e
Noreddine Hakkaoui <nhakkaou@e2r3p1.1337.ma>
2022-12-22 16:36:14 +0100

    struct files

---
+++ level1/source.c
@@ -0,0 +1,11 @@
+int main(int ac, char **av)
+{
+    int iVar1;
+    iVar1 = atoi(av[1]);
+    if (iVar1 == 0x1a7) // 423
+        execv("/bin/sh", *av);
+    else
+        write(1, "No !\n", 5);
+
+    return 0;
+}
