e5c134b4f975c0f39a5ec8eb37cca662e1515480
mourasoft <mourad.meski@gmail.com>
2023-03-15 17:41:56 +0100

    source.c level4

---
+++ level4/source.c
@@ -0,0 +1,23 @@
+char *p(char *arg)
+{
+    return printf(arg);
+}
+
+int n()
+{
+
+    char *ptr
+
+        fgets(ptr, 0x200, stdin);
+    p(ptr);
+    if (*0x8049810 == 0x1025544)
+    {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+    return;
+}
+
+int main()
+{
+    return n();
+}
